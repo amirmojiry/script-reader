803626bd0b8c17726da219d88c9774c1271038ec
Amirhossein Mojiri Foroushani <amojiry@gmail.com>
2026-09-17 14:03:52 +0300

    feat: assemble supplied Yasrebi plays (#7)

---
+++ src/data/yasrebi/index.ts
@@ -0,0 +1,37 @@
+import { buildYasrebiPlay } from './build'
+import type { RawPlaySource } from './types'
+import { records01 as banoo01 } from './banoo_va_marde_mordeh/records01'
+import { records02 as banoo02 } from './banoo_va_marde_mordeh/records02'
+import { records03 as banoo03 } from './banoo_va_marde_mordeh/records03'
+import { records04 as banoo04 } from './banoo_va_marde_mordeh/records04'
+import { records01 as jenayat01 } from './jenayat_va_mokafeat/records01'
+import { records02 as jenayat02 } from './jenayat_va_mokafeat/records02'
+import { records01 as akset01 } from './akset_dasteh_jamei_ba_khanom_bozorg/records01'
+import { records02 as akset02 } from './akset_dasteh_jamei_ba_khanom_bozorg/records02'
+import { records03 as akset03 } from './akset_dasteh_jamei_ba_khanom_bozorg/records03'
+import { records04 as akset04 } from './akset_dasteh_jamei_ba_khanom_bozorg/records04'
+
+export const yasrebiAuthor = 'چیستا یثربی'
+
+export const yasrebiSources: RawPlaySource[] = [
+  {
+    id: 'banoo_va_marde_mordeh',
+    title: 'بانو و مرد مرده',
+    characters: ['زن', 'دختر', 'داوود', 'شهردار', 'دوست دختر'],
+    records: [...banoo01, ...banoo02, ...banoo03, ...banoo04]
+  },
+  {
+    id: 'jenayat_va_mokafeat',
+    title: 'جنایت و مکافات',
+    characters: ['راسکلنیکوف', 'سونیا', 'مادر', 'پیرزن', 'دونیا', 'بازپرس', 'کاترینا', 'پولیا'],
+    records: [...jenayat01, ...jenayat02]
+  },
+  {
+    id: 'akset_dasteh_jamei_ba_khanom_bozorg',
+    title: 'عکس دسته جمعی با خانم بزرگ',
+    characters: ['ناصر', 'نادر', 'اکرم', 'ساناز', 'یعقوب'],
+    records: [...akset01, ...akset02, ...akset03, ...akset04]
+  }
+]
+
+export const yasrebiBundledPlays = yasrebiSources.map((source) => buildYasrebiPlay(source, yasrebiAuthor))
